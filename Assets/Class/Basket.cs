303dbc9b72043c2b5eb4a9de06b857d71f04a994
Zakaria Mrad <1839783@cstj.qc.ca>
2022-12-02 10:24:47 -0500

    all

---
+++ Assets/Class/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tp_Synthèse_B
+{
+    public class Basket 
+    {
+        public int Id { get; set; }
+        public List<KeyValuePair<int, Product>> Products { get; set; } //int will be the product quantity
+        public Double TotalPrice { get; set; }
+
+        static double taxes = 0.0015;
+    }
+}
---
+++ Assets/Class/Clothes.cs
@@ -0,0 +1,11 @@
+﻿namespace Tp_Synthèse_B
+{
+    public class Clothes:Product
+    {
+        public int Id { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string Image { get; set; }
+
+    }
+}
